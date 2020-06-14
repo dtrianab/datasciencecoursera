767abb22c4c5c686d850fd75f3463a63f535333b
dtrianab <da.trianab@gmail.com>
2020-06-14 11:14:28 +0200

    Update L2_005_test_assignment2.R

--- Code/L2_005_test_assignment2.R
+++ Code/L2_005_test_assignment2.R
@@ -1,39 +1,32 @@
 #Programming Assignment 2: Lexical Scoping
 
-#Functions must be on the same directory of this file:
-source("makeCacheMatrix.R")
-source("cacheSolve.R")
+#Functions file "cachematrix.R" must be on the same directory of this file:
+source("cachematrix.R")
 library(matlib)
+
 
 #Test list of squared matrices with random values
 list_mat<-rep(
         lapply(rep(2:6, each = 6, times = 6),
-        new_mat<-function(k){q<-array(runif(k^2),c(k,k))}),
+               new_mat<-function(k){q<-array(runif(k^2),c(k,k))}),
         each=6,times=6)
-
-#Result of inverses by using cache
-res_cache<-list()
+res_cache<-list() #Result of inverses by using cache
 
 #Block to measure calculations with cache >> 
 #---------------------------------------------------
 start_time_cache <- Sys.time()
 x<-makeCacheMatrix(list_mat[[1]])
-
 for(i in 1:length(list_mat)){
         #Check if stored 
         if(!identical(x$get(),list_mat[[i]])){
-                        x<-makeCacheMatrix(list_mat[[i]])
-                }
+                x<-makeCacheMatrix(list_mat[[i]])
+        }
         res_cache[[i]]<-cacheSolve(x)
         #print(cacheSolve(x))
 }
 end_time_cache <- Sys.time()
 t_cache<-end_time_cache-start_time_cache
-print(paste("Roughly time execution of inverse calculatio = ",t_cache))
 #---------------------------------------------------
-
-
-
 
 
 #Usual calculation execution time without cache 
@@ -48,7 +41,9 @@ t<-end_time-start_time
 #Test if inverse calculated properly
 inv_ok<-sum(round(list_mat[[i]]%*%res[[1]]))==ncol(list_mat[[1]])
 for(i in 2:length(list_mat)){inv_ok<-c(inv_ok, sum(round(list_mat[[i]]%*%res[[i]]))==ncol(list_mat[[i]]))}
-print(paste("Nuber of Inverse correctly calculated =",sum(inv_ok)))
 
-#Calculate Time reduced 
+#Performance > Calculate Time reduced 
+print(paste("Number of Inverses correctly calculated =",sum(inv_ok)))
+print(paste("Inverse cal. time without cache = ",t))
+print(paste("Inverse cal. time with Cache= ",t_cache))
 print(paste("Time Reduced with Cache:",t-t_cache))
